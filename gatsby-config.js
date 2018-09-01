fa4fb937d8af955cd1880a299a92e0ec75db07ca
J.B. Chaykowsky <jbchaykowsky@JBs-MacBook-Pro.local>
2018-08-31 23:31:57 -0500

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'UEXUI',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to design and code.</h1>
+    <p>Testing!!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
