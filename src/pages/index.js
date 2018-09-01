0566db5b2fde59a12f436c2d5506c9054c0db800
J.B. Chaykowsky <jbchaykowsky@JBs-MacBook-Pro.local>
2018-08-31 23:34:22 -0500

    Just updated some text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Learn to design and code.</h1>
     <p>Testing!!</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch Video!!</Link>
   </div>
 )
 
