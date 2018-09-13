01657e9246b08c0f27fec060dc39dd3df77eaee1
J.B. Chaykowsky <jbchaykowsky@JBs-MacBook-Pro.local>
2018-09-12 23:01:52 -0500

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: '200 Drinks',
   },
-  plugins: ['gatsby-plugin-react-helmet'],[`gatsby-plugin-styled-components`],
+  plugins: ['gatsby-plugin-react-helmet'],
 }
